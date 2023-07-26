6a84d415bbc01303bcaa642ca877619886d3fcfb
Greed <suby0475@gmail.com>
2023-07-27 03:37:02 +0900

    change todo list icons and hover effect

--- Naughtya/Projects/Features/CoreFeature/Sources/Presentation/Views/TodoItem/TodoItemView.swift
+++ Naughtya/Projects/Features/CoreFeature/Sources/Presentation/Views/TodoItem/TodoItemView.swift
@@ -43,35 +43,47 @@ public struct TodoItemView: View {
             let absoluteRect = geometry.frame(in: .global)
             VStack {
                 Spacer()
-                HStack(alignment: .center) {
-                    Text("🖱️")
-                        .opacity(isHovered ? 1 : 0)
-                        .animation(.easeOut, value: isHovered)
-                    Button(todo.isCompleted ? "✅" : "◻️") {
-                        toggleCompleted()
+                ZStack {
+                    if isHovered {
+                        Color.customGray5
                     }
-                    .buttonStyle(.borderless)
-                    if !isBacklog {
-                        Text("[\(todo.category)]")
-                            .font(.headline)
-                    }
-                    ZStack {
-                        TextField(text: $title) {
-                            placeholder
+                    HStack(alignment: .center) {
+                        Text("🖱️")
+                            .opacity(isHovered ? 1 : 0)
+                            .animation(.easeOut, value: isHovered)
+                        Button(action: {
+                            toggleCompleted()
+                        }, label: {
+                            Image(systemName: todo.isCompleted ? "checkmark.square" : "square")
+                                .foregroundColor(todo.isCompleted ? Color.customGray3 : Color.pointColor)
+                                .font(.system(size: 22))
+                        })
+                        .buttonStyle(.borderless)
+                        if !isBacklog {
+                            Text("[\(todo.category)]")
+                                .font(
+                                    Font.custom("Apple SD Gothic Neo", size: 16)
+                                        .weight(.bold)
+                                )
                         }
-                        .textFieldStyle(.plain)
-                        Color.white
-                            .opacity(isBlockedToEdit ? 0.01 : 0)
+                        Spacer().frame(width: 4)
+                        ZStack {
+                            TextField(text: $title) {
+                                placeholder
+                            }
+                            .font(Font.custom("Apple SD Gothic Neo", size: 16))
+                            .textFieldStyle(.plain)
+                        }
+                        Button("🔄") {
+                            toggleDaily()
+                        }
+                        .buttonStyle(.borderless)
+                        Button("🚮") {
+                            delete()
+                        }
+                        .buttonStyle(.borderless)
+                        Spacer()
                     }
-                    Button("🔄") {
-                        toggleDaily()
-                    }
-                    .buttonStyle(.borderless)
-                    Button("🚮") {
-                        delete()
-                    }
-                    .buttonStyle(.borderless)
-                    Spacer()
                 }
                 Spacer()
             }
@@ -92,7 +104,6 @@ public struct TodoItemView: View {
             }
         }
         .frame(height: 40)
-        .background(.white)
         .opacity(opacity)
         .gesture(
             DragGesture()
@@ -135,7 +146,7 @@ public struct TodoItemView: View {
 
     private var placeholder: some View {
         Text("Todo")
-            .foregroundColor(.gray)
+            .foregroundColor(Color.customGray3)
     }
 
     private var opacity: CGFloat {
