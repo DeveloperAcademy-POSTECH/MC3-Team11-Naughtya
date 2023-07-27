7ba61957852685c51113e6be69657e0b938bd731
hyeok3013 <kdh301397@gmail.com>
2023-07-27 20:56:03 +0900

    프로젝트 투두 추가 문구 버튼 수정

--- Naughtya/Projects/Features/CoreFeature/Sources/Presentation/Views/ProjectItem/ProjectItemView.swift
+++ Naughtya/Projects/Features/CoreFeature/Sources/Presentation/Views/ProjectItem/ProjectItemView.swift
@@ -64,14 +64,41 @@ public struct ProjectItemView: View {
         .padding(.top, 25)
         .padding(.bottom, 10)
 
-        TodoListView(
-            section: project.entity,
-            todos: todos,
-            isBlockedToEdit: searchManager.isSearching
-        )
-        Button("Todo 추가") {
-            appendNewTodo(project: project.entity)
+        VStack {
+
+            TodoListView(
+                section: project.entity,
+                todos: todos,
+                isBlockedToEdit: searchManager.isSearching
+            )
+
+            HStack(alignment: .center, spacing: 4) {
+
+                Text("􀅼")
+                  .font(
+                    Font.custom("SF Pro", size: 22)
+                      .weight(.light)
+                  )
+                  .foregroundColor(Color.customGray3)
+
+                Text("프로젝트 할 일을 추가해보세요.")
+                  .font(Font.custom("Apple SD Gothic Neo", size: 14))
+                  .foregroundColor(Color.customGray3)
+                  .frame(width: 184, height: 16, alignment: .leading)
+            }
+            .background(
+                RoundedRectangle(cornerRadius: 14)
+                    .fill(Color.white.opacity(0.0001))
+                    .frame(minWidth: 1000, alignment: .leading)
+            )            .padding(.top, -100)
+            .frame(maxWidth: .infinity, alignment: .center)
+
+            .onTapGesture {
+                appendNewTodo(project: project.entity)
+            }
+
         }
+
     }
 
     private var todos: [TodoModel] {
