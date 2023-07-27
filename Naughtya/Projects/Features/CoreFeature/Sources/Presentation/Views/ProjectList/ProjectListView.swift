e318a980ccb93ce119b66993b332ae79b42dc81e
hyeok3013 <kdh301397@gmail.com>
2023-07-27 21:34:29 +0900

    projectListView 변경

--- Naughtya/Projects/Features/CoreFeature/Sources/Presentation/Views/ProjectList/ProjectListView.swift
+++ Naughtya/Projects/Features/CoreFeature/Sources/Presentation/Views/ProjectList/ProjectListView.swift
@@ -11,43 +11,47 @@ import SwiftUI
 public struct ProjectListView: View {
     private static let projectUseCase: ProjectUseCase = DefaultProjectUseCase()
     public let projects: [ProjectModel]
-
+    
     public init(projects: [ProjectModel] = []) {
         self.projects = projects
     }
-
+    
     @State private var showModal = false
-
+    
     public var body: some View {
         ZStack {
             if projects.isEmpty {
-
+                
                 VStack {
-                    HStack(alignment: .center) {
-
-                            Text("프로젝트가 없습니다.")
-                              .font(
-                                Font.custom("Apple SD Gothic Neo", size: 12)
-                                  .weight(.semibold)
-                              )
-                              .multilineTextAlignment(.center)
-                              .foregroundColor(Color(red: 0.51, green: 0.51, blue: 0.51))
+                    List {
+                        Section(header: ListHeaderView()) {
+                            HStack(alignment: .center) {
+                                
+                                Text("프로젝트가 없습니다.")
+                                    .font(
+                                        Font.custom("Apple SD Gothic Neo", size: 12)
+                                            .weight(.semibold)
+                                    )
+                                    .multilineTextAlignment(.center)
+                                    .foregroundColor(Color(red: 0.51, green: 0.51, blue: 0.51))
+                            }
+                            
+                            .padding(.horizontal, 20)
+                            .padding(.vertical, 0)
+                            .frame(maxWidth: .infinity, minHeight: 68, maxHeight: 68, alignment: .center)
+                            .cornerRadius(5)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 5)
+                                    .inset(by: 0.5)
+                                    .stroke(Color(red: 0.31, green: 0.31, blue: 0.31), lineWidth: 1)
+                            ).padding(20)
+                            Spacer()
+                        }
                     }
-
-                    .padding(.horizontal, 20)
-                    .padding(.vertical, 0)
-                    .frame(maxWidth: .infinity, minHeight: 68, maxHeight: 68, alignment: .center)
-                    .cornerRadius(5)
-                    .overlay(
-                      RoundedRectangle(cornerRadius: 5)
-                        .inset(by: 0.5)
-                        .stroke(Color(red: 0.31, green: 0.31, blue: 0.31), lineWidth: 1)
-                    ).padding(20)
-                    Spacer()
-                }
-
+                }.background()
+                
             } else {
-
+                
                 VStack(spacing: 16) {
                     List {
                         Section(header: ListHeaderView()) {
@@ -58,7 +62,9 @@ public struct ProjectListView: View {
                     }
                     .listStyle(.plain)
                 }
+                
             }
+            
             VStack(alignment: .center, spacing: 10) {
                 Spacer()
                 Button(action: {
@@ -69,7 +75,7 @@ public struct ProjectListView: View {
                         Text("+")
                             .font(Font.custom("SF Pro", size: 20))
                             .foregroundColor(.white)
-
+                        
                         Text("Add project")
                             .lineLimit(1)
                             .font(Font.custom("SF Pro", size: 14))
@@ -98,7 +104,7 @@ public struct ProjectListView: View {
 
 struct ListHeaderView: View {
     private static let dummyDataGenerator: DummyDataGenerator = .shared
-
+    
     @State private var showModal = false
     var body: some View {
         HStack {
