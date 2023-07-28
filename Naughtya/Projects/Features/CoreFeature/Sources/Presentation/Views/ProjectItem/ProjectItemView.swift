d63c095c8114abff1876a43d6032504a7926f814
Taehyun <taehyunjo97@gmail.com>
2023-07-28 14:51:48 +0900

    Modify project todo list header

--- Naughtya/Projects/Features/CoreFeature/Sources/Presentation/Views/ProjectItem/ProjectItemView.swift
+++ Naughtya/Projects/Features/CoreFeature/Sources/Presentation/Views/ProjectItem/ProjectItemView.swift
@@ -21,70 +21,59 @@ public struct ProjectItemView: View {
 
     public var body: some View {
         HStack {
-            VStack(alignment: .leading, spacing: 18) {
+            VStack(alignment: .leading, spacing: 10) {
                 Text(project.category)
-                    .font(
-                        Font.custom("SF Pro", size: 24)
-                            .weight(.bold)
-                    )
                     .foregroundColor(.white)
+                    .font(.custom("SF Pro", size: 32).weight(.bold))
+                    .lineLimit(1)
+                    .frame(height: 35)
                 HStack {
-                    Rectangle()
-                        .foregroundColor(.clear)
-                        .frame(width: 22, height: 22)
-                        .background(Color.customGray1)
-                    if let goals = project.goals, !goals.isEmpty {
+                    Text("🎯")
+                    if let goals = project.goals,
+                       !goals.isEmpty {
                         Text(goals)
-                            .font(Font.custom("SF Pro", size: 14))
-                            .foregroundColor(Color.customGray2)
-                            .frame(width: 360, alignment: .leading)
                     } else {
                         Text("(선택) 목표를 입력해 보세요.")
-                            .font(Font.custom("SF Pro", size: 14))
-                            .foregroundColor(Color.customGray2)
-                            .frame(width: 360, alignment: .leading)
                     }
                 }
+                .foregroundColor(Color.customGray2)
+                .font(.custom("SF Pro", size: 20))
+                .frame(minHeight: 32)
             }
-            .padding(.horizontal, 20)
-            .frame(width: 270, alignment: .topLeading)
             Spacer()
         }
         .padding(.leading, 20)
         .padding(.top, 15)
         .padding(.bottom, 10)
-
         VStack {
             TodoListView(
                 section: project.entity,
                 todos: todos,
                 isBlockedToEdit: filterManager.isSearching
             )
-
             HStack(alignment: .center, spacing: 4) {
                 Text("􀅼")
-                  .font(
-                    Font.custom("SF Pro", size: 22)
-                      .weight(.light)
-                  )
-                  .foregroundColor(Color.customGray3)
-
+                    .font(
+                        Font.custom("SF Pro", size: 22)
+                            .weight(.light)
+                    )
+                    .foregroundColor(Color.customGray3)
                 Text("프로젝트 할 일을 추가해보세요.")
-                  .font(Font.custom("Apple SD Gothic Neo", size: 14))
-                  .foregroundColor(Color.customGray3)
-                  .frame(width: 184, height: 16, alignment: .leading)
+                    .font(Font.custom("Apple SD Gothic Neo", size: 14))
+                    .foregroundColor(Color.customGray3)
+                    .frame(width: 184, height: 16, alignment: .leading)
             }
             .background(
                 RoundedRectangle(cornerRadius: 14)
                     .fill(Color.white.opacity(0.0001))
                     .frame(minWidth: 1000, alignment: .leading)
-            )            .padding(.top, -100)
+            )
+            .padding(.top, -100)
             .frame(maxWidth: .infinity, alignment: .center)
             .onTapGesture {
                 appendNewTodo(project: project.entity)
             }
         }
-
     }
 
     private var todos: [TodoModel] {
