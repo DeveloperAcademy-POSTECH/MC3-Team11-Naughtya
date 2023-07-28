9a7de7e7d8fdc81a656cc3e6337ff0d0daa5b232
Taehyun <taehyunjo97@gmail.com>
2023-07-28 17:27:49 +0900

    Design project card view

--- Naughtya/Projects/Features/CoreFeature/Sources/Presentation/Views/ProjectCardView/ProjectCardView.swift
+++ Naughtya/Projects/Features/CoreFeature/Sources/Presentation/Views/ProjectCardView/ProjectCardView.swift
@@ -10,113 +10,105 @@ import SwiftUI
 
 struct ProjectCardView: View {
     private static let projectUseCase: ProjectUseCase = DefaultProjectUseCase()
+
     public let project: ProjectModel
-
-    private let cornerRadius: CGFloat = 5
     @State private var showModal = false
-
-    var projectEndDay: Date? {
-        project.endedAt
-    }
 
     var body: some View {
         ZStack(alignment: .topLeading) {
-            if project.isBookmarked == true {
-                Image(systemName: "pin.fill")
-                    .rotationEffect(.degrees(30))
-                    .font(.system(size: 9))
-                    .foregroundColor(.pointColor)
-                    .onTapGesture {
-                        toggleIsBookmarked()
-                    }
-                    .offset(x: 12, y: 16)
-                    .zIndex(1)
-            }
             HStack {
-                VStack(alignment: .leading) {
-                    Text(project.category)
-                    .font(
-                    Font.custom("Apple SD Gothic Neo", size: 24)
-                    .weight(.medium)
-                    )
-                        .foregroundColor(.white)
-                    if let projectEndDay = projectEndDay {
-                        Text("- \(changeDateFormat(projectEndDay: projectEndDay))")
-                            .font(
-                                Font.custom("Apple SD Gothic Neo", size: 12)
-                                    .weight(.semibold)
-                            )
-                            .foregroundColor(Color.customGray2)
-                    }
-                }
+                contentView
                 Spacer()
-                VStack {
-                    HStack(alignment: .firstTextBaseline, spacing: 0.0) {
-                        Text("\(project.completedTodos.count)")
-                            .font(
-                                Font.custom("Apple SD Gothic Neo", size: 24)
-                                    .weight(.semibold)
-                            )
-                            .multilineTextAlignment(.trailing)
-                            .foregroundColor(Color.pointColor)
-                        Text("/\(project.todos.count)")
-                            .font(
-                                Font.custom("Apple SD Gothic Neo", size: 16)
-                                    .weight(.regular)
-                            )
-                            .multilineTextAlignment(.trailing)
-                            .foregroundColor(.white)
-                    }
-                }
+                todosCountView
             }
-            .zIndex(0)
             .frame(height: 68)
             .padding(.leading, 25)
             .padding(.trailing, 15)
             .background(
-                RoundedRectangle(cornerRadius: cornerRadius)
+                RoundedRectangle(cornerRadius: 5)
                     .fill(project.isSelected ? Color.customGray4 : Color.white.opacity(0.001))
             )
-            .onTapGesture {
+            if project.isBookmarked {
+                bookmarkIndicator
+            }
+        }
+        .onTapGesture {
+            Task {
+                try await Self.projectUseCase.toggleSelected(
+                    project.entity,
+                    isSelected: !project.isSelected
+                )
+            }
+        }
+        .contextMenu {
+            Button {
+                showModal = true
+            } label: {
+                Label("Modify", systemImage: "pencil")
+                    .labelStyle(.titleAndIcon)
+            }
+            Button {
                 Task {
-                    try await Self.projectUseCase.toggleSelected(
+                    try await Self.projectUseCase.toggleIsBookmarked(
                         project.entity,
-                        isSelected: !project.isSelected
-                    )
+                        isBookmarked: !project.isBookmarked)
                 }
+            } label: {
+                Label("Bookmark", systemImage: "bookmark")
+                    .labelStyle(.titleAndIcon)
             }
-            .contextMenu {
-                Button {
-                    self.showModal = true
-                } label: {
-                    Label("Modify", systemImage: "pencil")
-                        .labelStyle(.titleAndIcon)
+            Divider()
+            Button {
+                Task {
+                    try await Self.projectUseCase.delete(project.entity)
                 }
-                Button {
-                    Task {
-                        try await Self.projectUseCase.toggleIsBookmarked(
-                            project.entity,
-                            isBookmarked: !project.isBookmarked)
-                    }
-                } label: {
-                    Label("Bookmark", systemImage: "bookmark")
-                        .labelStyle(.titleAndIcon)
-                }
-                Divider()
-                Button {
-                    Task {
-                        try await Self.projectUseCase.delete(project.entity)
-                    }
-                } label: {
-                    Label("삭제", systemImage: "trash")
-                        .labelStyle(.titleAndIcon)
-                }
+            } label: {
+                Label("삭제", systemImage: "trash")
+                    .labelStyle(.titleAndIcon)
             }
-            .sheet(isPresented: self.$showModal) {
-                ProjectSetModalView(project: project)
+        }
+        .sheet(isPresented: $showModal) {
+            ProjectSetModalView(project: project)
+        }
+    }
+
+    private var contentView: some View {
+        VStack(alignment: .leading) {
+            Text(project.category)
+                .font(Font.custom("SF Pro", size: 24).weight(.medium))
+                .foregroundColor(.white)
+            if let projectEndDay = project.endedAt {
+                Text("- \(changeDateFormat(projectEndDay: projectEndDay))")
+                    .font(Font.custom("SF Pro", size: 12).weight(.semibold))
+                    .foregroundColor(Color.customGray2)
             }
         }
     }
+
+    private var todosCountView: some View {
+        HStack(alignment: .firstTextBaseline, spacing: 0) {
+            Text("\(project.completedTodos.count)")
+                .font(Font.custom("SF Pro", size: 24).weight(.semibold))
+                .foregroundColor(Color.pointColor)
+            Text("/\(project.todos.count)")
+                .font(Font.custom("SF Pro", size: 16).weight(.regular))
+                .foregroundColor(.white)
+        }
+        .multilineTextAlignment(.trailing)
+    }
+
+    private var bookmarkIndicator: some View {
+        Image(systemName: "pin.fill")
+            .rotationEffect(.degrees(30))
+            .font(.system(size: 9))
+            .foregroundColor(.pointColor)
+            .onTapGesture {
+                toggleIsBookmarked()
+            }
+            .offset(x: 12, y: 16)
+            .zIndex(1)
+    }
+
     func toggleIsBookmarked() {
         Task {
             try await Self.projectUseCase.toggleIsBookmarked(
@@ -131,5 +123,4 @@ struct ProjectCardView: View {
         let formattedDate = dateFormatter.string(from: projectEndDay)
         return formattedDate
     }
-
 }
