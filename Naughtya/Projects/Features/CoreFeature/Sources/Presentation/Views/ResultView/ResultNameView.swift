aa803d6d607b9185712ba046183842c128d3ad44
hyeok3013 <kdh301397@gmail.com>
2023-08-01 22:27:32 +0900

    컴파일 오류 수정

--- Naughtya/Projects/Features/CoreFeature/Sources/Presentation/Views/ResultView/ResultNameView.swift
+++ Naughtya/Projects/Features/CoreFeature/Sources/Presentation/Views/ResultView/ResultNameView.swift
@@ -27,20 +27,20 @@ public struct ResultNameView: View {
     public var body: some View {
         switch selectedNum {
         case 1:
-                    HStack {
-            VStack(alignment: .leading, spacing: 0) {
-                Text("\(projectResult.projectName) 프로젝트")
-                    .lineLimit(1)
-                    .font(
-                        .system(size: 32 * (geometry.size.width/1512))
+            HStack {
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("\(projectResult.projectName) 프로젝트")
+                        .lineLimit(1)
+                        .font(
+                            .system(size: 32 * (geometry.size.width/1512))
                             .weight(.bold)
-                    )
-                    .foregroundColor(Color(red: 0.88, green: 0.88, blue: 0.88))
+                        )
+                        .foregroundColor(Color(red: 0.88, green: 0.88, blue: 0.88))
 
-                Text("\(projectResult.daysInProject)일간의 능력로그") // 종료된 프로젝트와 데이터 연결
-                    .lineLimit(1)
-                    .font(
-                        .system(size: 28 * (geometry.size.width/1512))
+                    Text("\(projectResult.daysInProject)일간의 능력로그") // 종료된 프로젝트와 데이터 연결
+                        .lineLimit(1)
+                        .font(
+                            .system(size: 28 * (geometry.size.width/1512))
                             .weight(.medium)
                         )
                         .foregroundColor(Color(red: 0.51, green: 0.51, blue: 0.51))
@@ -70,15 +70,16 @@ public struct ResultNameView: View {
 
                         .clipped()
 
-                Text("에필로그")
-                    .font(
-                        .system(size: 16.17978 * (geometry.size.height/892))
+                    Text("에필로그")
+                        .font(
+                            .system(size: 16.17978 * (geometry.size.height/892))
                             .weight(.semibold)
-                    )
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(.white)
-                    .padding(.top, 5 * (geometry.size.height/892))
+                        )
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.white)
+                        .padding(.top, 5 * (geometry.size.height/892))
 
+                }
             }
 
             .padding(.horizontal, 6)
